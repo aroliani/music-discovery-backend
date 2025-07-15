ed846ad6dee3a1029b53bb105c4d2059e4587054
aroliani <148553795+aroliani@users.noreply.github.com>
2025-07-15 22:32:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -19,9 +19,20 @@ import { isSameUserValidator } from "./validators/post.validator.js";
 const app = express();
 
 // Middleware
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://music-project-frontend.netlify.app' 
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   })
 );
